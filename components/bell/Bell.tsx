4bab6ff935b37aef493981808b24ce3acca1f3fa
oussama <54941790+darth-liutas@users.noreply.github.com>
2024-03-02 15:09:25 +0100

    uploaded some comps
    I am still trying to figure out how github desktop works, I'm getting "unknown error" so I am uploading files manually to see if that fixes it

---
+++ components/bell/Bell.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Form from './Form.tsx'
+import { useState } from 'react'
+import {faBell} from "@fortawesome/free-solid-svg-icons"
+export default function Home() {
+  var number_of_notifications=10;
+  return (
+   <div className="flex items-center justify-center h-screen">
+       <div className="flex items-center justify-center size-16 bg-blue-500 rounded">
+       <div className="relative">
+       <div className="bg-red-500 size-6 rounded-full flex item-center justify-center text-white circle">{number_of_notifications}</div>
+       <FontAwesomeIcon icon={faBell} className="text-2xl"  />
+       </div>
+       </div>
+   </div>
+  );
+}
+
---
+++ components/pop up/app/Form.tsx
@@ -0,0 +1,22 @@
+
+export default function Form({send}){
+const handleClick = () => {
+     var data="doesn't matter"
+     send(data);
+  };
+
+return (
+<div className="shade">
+<div className="form">
+<p className="exit" onClick={handleClick}>X</p>
+<div className="inner-form">
+<input type="text" />
+<input type="text" />
+<input type="text" />
+</div>
+</div>
+</div>
+)
+}
+
+
