70c4302f20d531a3f1dd9c228f8c0a1b426f0d1a
HeliosGreen <153565457+HeliosGreen@users.noreply.github.com>
2024-05-27 04:56:50 +0100

    made create warehouse api request

--- backend/WareLink/API_DATABASE_REQUESTS/urls.py
+++ backend/WareLink/API_DATABASE_REQUESTS/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('registeration', registeration, name='registeration'),
     path('populate_account/', populate_account, name='populate_account'),
     path('edit_warehouse', edit_warehouse, name='edit_warehouse'),
+    path('create_warehouse', edit_warehouse, name='create_warehouse'),
+    path('get_all_json_data_for_three_consumer', get_all_json_data_for_three_consumer, name='get_all_json_data_for_three_consumer'),
 ]
