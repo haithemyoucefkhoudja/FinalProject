34d328a4d1377995b9e50297212171337fae1801
haithemyoucefkhoudja <youcefkhoudjahaithem@gmail.com>
2024-06-04 13:20:00 +0100

    dasjddasdkf

--- src/app/Dashboard/layout.tsx
+++ src/app/Dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import { SidebarProvider } from "@/context/SideBarContext"
 import { Toaster } from "react-hot-toast";
 
-import SideBar  from "@/components/component/SideBar"
+import {SideBar}  from "@/components/component/SideBar"
 import { ReactNode } from "react"
 import { CheckDataProvider } from "@/context/checkProvider";
 export default function DashboardLayout({
